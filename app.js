b4dca66f255080cc8f3e6229c8182bf99e66ace7
erdenejargal <jasmin.marin2021@gmail.com>
2024-02-16 08:40:45 +0100

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Shooni ali talaaraa buusaniig hadgalah huvisagch, 1-6 gesen utgiig ene huvisagchid sanamsarguigeer uusgej ugnu.
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 // Togloom ehlehed beldey.
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('.dice').style.display = "none";
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+document.getElementById('score-0').textContent = "0"; // olon fain hurdan hurdan. 
+document.getElementById('score-1').textContent = "0";
+document.getElementById('current-0').textContent = "0";
+document.getElementById('current-1').textContent = "0";
+var diceDom = document.querySelector('.dice');
+diceDom.style.display = "none";
 
-
-
-console.log('Shoo: ' + dice);
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + '.png';
+});
